dd1e549c57f864c58b4fc06b5536fb978d82bc48
Sergey <servitant@gmail.com>
2020-12-18 09:01:11 +0200

    First Commit

---
+++ src/com/max/idea/Main.java
@@ -0,0 +1,19 @@
+package com.max.idea;
+
+public
+class Main {
+    public static
+    void main ( String[] args ) {
+        int a = 55;
+        int b = 45;
+        if ( a == b ) {
+            System.out.println ( "a равно b" );
+        }
+        else if ( a < b ) {
+            System.out.println ( "a меньше b" );
+        }
+        else {
+            System.out.println ( "a больше b" );
+        }
+    }
+}
